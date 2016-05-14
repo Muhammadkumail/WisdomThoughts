c0835dcec8b036e7059a2b30dcf27ec56fbf2746
Muhammad kumail <muhammad.kumail449@gmail.com>
2016-05-14 23:37:47 +0500

    languages added

---
+++ app/src/main/java/net/mk786110/wisdomthoughts/ArabiActivity.java
@@ -0,0 +1,13 @@
+package net.mk786110.wisdomthoughts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ArabiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_arabi);
+    }
+}
--- app/src/main/java/net/mk786110/wisdomthoughts/HomeActivity.java
+++ app/src/main/java/net/mk786110/wisdomthoughts/HomeActivity.java
@@ -1,8 +1,10 @@
 package net.mk786110.wisdomthoughts;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
@@ -14,5 +16,32 @@ public class HomeActivity extends AppCompatActivity {
         Typeface myTypeFace=Typeface.createFromAsset(getAssets(),"funRaiser.ttf");
         TextView textView=(TextView)findViewById(R.id.home_actionBar_textView);
         textView.setTypeface(myTypeFace);
+    }
+
+    public void onClickEnglish(View view)
+    {
+        Intent mintent=new Intent(this,EnglishActivity.class);
+        startActivity(mintent);
+
+    }
+    public void onClickPersion(View view)
+    {
+
+        Intent mintent=new Intent(this,PersianActivity.class);
+        startActivity(mintent);
+
+    }
+    public void onClickUrdu(View view)
+    {
+
+        Intent mintent=new Intent(this,UrduActivity.class);
+        startActivity(mintent);
+
+    }
+    public void onClickArabi(View view)
+    {
+
+        Intent mintent=new Intent(this,ArabiActivity.class);
+        startActivity(mintent);
     }
 }
