bd86149e2e2b0e9ff4369dc705f5b84fcf3e3bc5
Muhammad kumail <muhammad.kumail449@gmail.com>
2016-05-14 14:00:09 +0500

    custom action bar created

--- app/src/main/java/net/mk786110/wisdomthoughts/HomeActivity.java
+++ app/src/main/java/net/mk786110/wisdomthoughts/HomeActivity.java
@@ -1,7 +1,9 @@
 package net.mk786110.wisdomthoughts;
 
+import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,8 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        Typeface myTypeFace=Typeface.createFromAsset(getAssets(),"funRaiser.ttf");
+        TextView textView=(TextView)findViewById(R.id.home_actionBar_textView);
+        textView.setTypeface(myTypeFace);
     }
 }
